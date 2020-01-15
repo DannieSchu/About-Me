77c294ae2eeb8ab696e2eea40035c44099f65936
Dannie Schumaker <draeschumaker@gmail.com>
2020-01-15 10:18:45 -0800

    make event listener function return responses according to user input

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const highLow = document.getElementById('high-low');
 const result = document.getElementById('result');
 
 // Randomly generate and store correct number
-let correctNumber = Math.floor(Math.random) * 20;
+let correctNumber = (Math.floor(Math.random() * 20));
 
 
 // Set initial state of count to 4
@@ -17,10 +17,6 @@ let count = 4;
 
 // Add event listener for button
 submitButton.addEventListener('click', () => {
-    console.log('button works!');
-
-    // Reduce count by 1
-    count--;
 
     // Change value of remaining attempts
     attemptsRemaining.textContent = count;
@@ -29,20 +25,26 @@ submitButton.addEventListener('click', () => {
     const guessStr = guessInput.value;
     const guess = parseInt(guessStr);
 
-    // Return result and estimate
-    if (compareNumbers (guess, correctNumber) === 1) {
-        highLow.textContent = 'too high.';
-        if (count === 0) {
-            result.textContent = 'You lost.';
-        } 
-    } else if (compareNumbers (guess, correctNumber) === -1) {
-        highLow.textContent = 'too low.';
-        if (count === 0) {
-            result.textContent = 'You lost.';
-        } 
-    } else {
+    if (compareNumbers(guess, correctNumber) === 1) {
+        // Return "too high" if guess > correct number
+        highLow.textContent = 'too high';
+        // Reduce count by 1
+        count--;
+    } else if (compareNumbers(guess, correctNumber) === -1) { 
+        // Return "too low" if guess < correct number
+        highLow.textContent = 'too low';
+        // Reduce count by 1
+        count--;
+    } else if (compareNumbers(guess, correctNumber) === 0) {
+        // Return "correct" and tell user they won if guess = correct number
         highLow.textContent = 'correct';
         result.textContent = 'You won!';
+        return;
+    }
+
+    if (count === 0 && compareNumbers(guess, correctNumber) !== 0) { 
+        result.textContent = 'You lost.';
+        return;
     }
 }
 );
