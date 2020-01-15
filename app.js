9052fad84facd1f9443e39c05506ae03e8ccbcf3
Dannie Schumaker <draeschumaker@gmail.com>
2020-01-15 09:47:00 -0800

    tests and js work

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import compareNumbers from './compare-numbers.js';
 // Get DOM elements
 const guessInput = document.getElementById('input-number');
 const submitButton = document.getElementById('submit-button');
-const attemptsRemaining = document.getElementById('attempt-remaining');
+const attemptsRemaining = document.getElementById('attempts-remaining');
 const highLow = document.getElementById('high-low');
 const result = document.getElementById('result');
 
@@ -17,6 +17,8 @@ let count = 4;
 
 // Add event listener for button
 submitButton.addEventListener('click', () => {
+    console.log('button works!');
+
     // Reduce count by 1
     count--;
 
@@ -27,26 +29,20 @@ submitButton.addEventListener('click', () => {
     const guessStr = guessInput.value;
     const guess = parseInt(guessStr);
 
-    // If compareNumbers returns 1
+    // Return result and estimate
     if (compareNumbers (guess, correctNumber) === 1) {
-        highLow.textContent = 'too high';
-        if (count === 0) {
-            result.textContent = "You lost.";
-            //disable game play?
-            return;
-        } 
-    } else if (compareNumbers (guess, correctNumber) === -1) {
-        highLow.textContent = 'too low';
+        highLow.textContent = 'too high.';
         if (count === 0) {
             result.textContent = 'You lost.';
-            //disable game play?
-            return;
+        } 
+    } else if (compareNumbers (guess, correctNumber) === -1) {
+        highLow.textContent = 'too low.';
+        if (count === 0) {
+            result.textContent = 'You lost.';
         } 
     } else {
         highLow.textContent = 'correct';
         result.textContent = 'You won!';
-        //disable game play?
-        return;
     }
 }
 );
