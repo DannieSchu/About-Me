361c4896f4485576797b90420c668cf1bb327e44
Dannie Schumaker <draeschumaker@gmail.com>
2020-01-22 11:06:56 -0800

    refactored app.js

--- app.js
+++ app.js
@@ -3,48 +3,53 @@ import compareNumbers from './compare-numbers.js';
 // Get DOM elements
 const guessInput = document.getElementById('input-number');
 const submitButton = document.getElementById('submit-button');
-const attemptsRemaining = document.getElementById('attempts-remaining');
+const triesRemaining = document.getElementById('attempts-remaining');
 const highLow = document.getElementById('high-low');
-const result = document.getElementById('result');
+const winLoss = document.getElementById('win-loss');
 
-// Randomly generate and store correct number
-let correctNumber = (Math.floor(Math.random() * 20));
+// Randomly generate and store correct number (use const because it will not change unless refreshed; would use let if had a reset option)
+const correctNumber = (Math.floor(Math.random() * 20));
 
+// Set initial state of tries to 4
+let tries = 4;
 
-// Set initial state of count to 4
-let count = 4;
-
+// Store variable to return loser message and disable button if run out of tries
+function sayYouLost(compareNumbers) {
+    if (tries === 0 && compareNumbers !== 0) { 
+        winLoss.textContent = `You lost. Try again later.`;
+        submitButton.disabled = true;
+    }
+}
 
 // Add event listener for button
 submitButton.addEventListener('click', () => {
+    // Reduce tries by 1
+    tries--;
 
-    // Change value of remaining attempts
-    attemptsRemaining.textContent = count;
-
+    // Change value of remaining tries
+    triesRemaining.textContent = tries;
+    
     // Convert string input to number
     const guessStr = guessInput.value;
     const guess = parseInt(guessStr);
 
-    if (compareNumbers(guess, correctNumber) === 1) {
+    // Save values of compare numbers to variable
+    const isHighLow = compareNumbers(guess, correctNumber);
+
+    if (isHighLow === 1) {
         // Return "too high" if guess > correct number
         highLow.textContent = `Guess again! ${guess} is too high.`;
-        // Reduce count by 1
-        count--;
-    } else if (compareNumbers(guess, correctNumber) === -1) { 
+
+    } else if (isHighLow === -1) { 
         // Return "too low" if guess < correct number
         highLow.textContent = `Guess again! ${guess} is too low.`;
-        // Reduce count by 1
-        count--;
-    } else if (compareNumbers(guess, correctNumber) === 0) {
+
+    } else if (isHighLow === 0) {
         // Return "correct" and tell user they won if guess = correct number
-        result.textContent = `${guess} is correct! You must have x-ray vision.`;
+        highLow.textContent = `${guess} is correct! You must have x-ray vision.`;
         submitButton.disabled = true;
-        result.textContent = `Congratulations!`;
+        winLoss.textContent = `Congratulations!`;
     }
-    // Return losing message and disable button if run out of tries
-    if (count === 0 && guess !== correctNumber) { 
-        result.textContent = `You lost. Try again later.`;
-        submitButton.disabled = true;
-    }
+    sayYouLost();
 }
 );
